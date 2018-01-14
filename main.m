c2b414a1de477773f51572437e60cc6bbb08e923
Haoran Li <lhr17@mails.tsinghua.edu.cn>
2018-01-14 20:11:46 +0800

    add five peaks figure

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 [sample, label, test_sample] = load_data();
+fft_sample = abs(fft(sample, 3000, 2));
 osample = sample;
 olabel = label;
 [sample, label, tsample, tlabel] = get_cv(sample, label, 0.7);
 addpath('data_process');
-fft_sample = abs(fft(sample, 3000, 2));
