aaadd0dc864e917b522aebab1eda739882356d76
Haoran Li <lhr17@mails.tsinghua.edu.cn>
2018-01-19 19:58:18 +0800

    remove irrelevant files, add result

--- bayesian/bayesian.m
+++
@@ -1,8 +0,0 @@
-% normalize train sample and test sample using pca_param
-% perform pca transformation using vec
-train_data = pca_trans(process(sample), vec, pca_param);
-test_data = pca_trans(process(test_sample), vec, pca_param);
-
-[pw, class_data] = classify(train_data, label);
-model = build_model(class_data);
-result = predict(test_data, test_label, model, pw);
